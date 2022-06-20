1ea7f8e7a02983a3c750b3c2cecfc57c6d71761f
xuanphuc97 <100549478+txphuc@users.noreply.github.com>
2022-06-21 04:01:38 +0700

    frontend

--- Frontend/README.md
+++ Frontend/README.md
@@ -1,4 +1,4 @@
-# myapp
+# frontend
 
 A new Flutter project.
 
--- frontend/android/app/src/main/kotlin/com/example/frontend/MainActivity.kt
+++ frontend/android/app/src/main/kotlin/com/example/frontend/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.flutter_demo
+package com.example.frontend
 
 import io.flutter.embedding.android.FlutterActivity
 
